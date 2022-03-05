bac22ea26c35b7456d505f5632a488094164e32d
raverena <raverena96@gmail.com>
2022-03-05 14:57:19 +0900

    [Add] redux

---
+++ src/store/modules/tasks.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { ITask } from 'types';
+import type { RootState } from 'store';
+
+interface ITaskState {
+  tasks: ITask[];
+}
+
+const initialState: ITaskState = {
+  tasks: [
+    { id: 1, todo: 'TDD 공부하기' },
+    { id: 2, todo: 'voice 구현하기' },
+  ],
+};
+
+export const taskSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<string>) => {
+      return {
+        ...state,
+        tasks: [...state.tasks, { id: 3, todo: action.payload }],
+      };
+    },
+  },
+});
